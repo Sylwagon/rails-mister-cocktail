217d87b31a261194d70eb9d4486cfec791ab79a4
sylvain charpiot <sylvain@drawn.fr>
2019-02-15 11:15:53 +0100

    structure models controller terminée

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,7 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :cocktail, uniqueness: { scope: :ingredient }
+  validates :ingredient, uniqueness: { scope: :cocktail }
   validates :cocktail, presence: true
   validates :ingredient, presence: true
   validates :description, presence: true
